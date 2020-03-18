5f27f1bb67f08908fb6f9487796b258c6016765b
Sourabh2901 <62259067+Sourabh2901@users.noreply.github.com>
2020-03-18 23:00:19 +0530

    Update implementation.java

--- LinkedList/implementation.java
+++ LinkedList/implementation.java
@@ -8,7 +8,6 @@ public class LinkedListUse {
 		
 		Node<Integer> head = null;
 		Node<Integer> tail = null;
-	//	System.out.println("Enter node data");
 		int data = s.nextInt();
 		
 		while(data != -1){
@@ -20,7 +19,6 @@ public class LinkedListUse {
 				tail.next = node;
 				tail = tail.next;
 			}
-		//	System.out.println("Enter node data");
 			data = s.nextInt();
 		}
 		s.close();
@@ -33,6 +31,25 @@ public class LinkedListUse {
 			System.out.print(temp.data+" ");
 			temp = temp.next;
 		}
+	}
+	
+	public static Node<Integer> insert(Node<Integer> head ,int data ,int pos){
+		Node<Integer> temp = head;
+		Node<Integer> newNode = new Node<>(data);
+		if(pos == 0){
+			newNode.next = head;
+			return newNode;
+		}
+		int i=0;
+		while(i != pos - 1){
+			temp = temp.next;
+			i++;
+		}
+		Node<Integer> temp2 = temp.next;
+		temp.next = newNode;
+		newNode.next = temp2;
+		
+		return head;
 	}
 	
 	public static void main(String[] args) {
